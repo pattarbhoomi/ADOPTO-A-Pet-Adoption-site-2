7ed546ba96d7c3c9a70ba789999ec78cdd44b979
Bhoomi Pattar <pattarbhoomi@gmail.com>
2026-04-26 22:41:29 +0530

    db.js

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'root',   
-  database: 'miniproject'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 module.exports = pool.promise();
